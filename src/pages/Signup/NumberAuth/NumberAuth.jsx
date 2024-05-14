c1762f0c5f6523da0f5bdbe1eb1c244ee4b89859
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-05-14 21:04:28 +0100

    new commit

---
+++ src/pages/Signup/NumberAuth/NumberAuth.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NumberAuth.css'
+// import Signup from '../Signup'
+
+const NumberAuth = ({numberauth}) => {
+  return (
+    <div>
+    
+
+      
+    </div>
+  )
+}
+
+export default NumberAuth

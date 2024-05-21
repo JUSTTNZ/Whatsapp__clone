84f3acfcff0a0c13dce9ead6662019e03529b80e
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-05-21 13:04:11 +0100

    updated the files

--- src/chats/UserMessage.js
+++ src/chats/UserMessage.js
@@ -1,15 +1,14 @@
 import React from "react";
 import '../css/UserMessage.css'
-import UserTextData from './UserTextData'
-import DisplayMessages from "./DisplayMessages";
+
 
 
 const UserMessage = ({ message, status, date, content,picture,user }) => (
   <div>
       {/* <DisplayMessages messages={UserTextData.messages}  /> */}
   <li>
-  <img src={picture}  className="chat-picture" />
-  <p>{user}</p>
+    <img src={picture} alt={picture} className="chat-picture" />
+    <p>{user}</p>
     <p>{message}</p>
     <p>Status: {status}</p>
     <p>Date: {new Date(date).toLocaleString()}</p>

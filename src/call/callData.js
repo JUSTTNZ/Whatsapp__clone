eafc5339e802d7d59977ed09c4a3004fd8630141
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-06-09 00:42:18 +0100

    added call functionality

--- src/call/callData.js
+++ src/call/callData.js
@@ -12,11 +12,11 @@ const callData = [
         picture: "https://images.pexels.com/photos/1459398/pexels-photo-1459398.jpeg?auto=compress&cs=tinysrgb&w=300"
     },
     {
-        id: "2",
-        caller: "Emily Clark",
-        picture: "https://images.pexels.com/photos/1459398/pexels-photo-1459398.jpeg?auto=compress&cs=tinysrgb&w=300"
+        id: "3",
+        caller: "Michael Brown",
+        picture: "https://images.pexels.com/photos/129105/pexels-photo-129105.jpeg?auto=compress&cs=tinysrgb&w=300"
     },
-    {
+    { 
         id: "2",
         caller: "Emily Clark",
         picture: "https://images.pexels.com/photos/1459398/pexels-photo-1459398.jpeg?auto=compress&cs=tinysrgb&w=300"

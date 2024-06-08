942cfb2235473cab5ac9c8d99d7b8a446448b6e4
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-06-08 22:39:28 +0100

    status folder updates

--- src/profile/verify.jsx
+++ src/profile/verify.jsx
@@ -1,4 +1,3 @@
-
 import React,{useState} from "react";
 import { useLocation,} from "react-router-dom";
 import { useNavigate } from "react-router-dom";
--- src/status/userStatus.jsx
+++ src/status/userStatus.jsx
@@ -8,19 +8,19 @@ const userStatus = ({picture,sender}) => {
         <span className='header'>Status</span>
         <div className='user'>
             <img src={userProfile} alt=''/>
-            <p>My status</p>
-            <p>No updates</p>
+            <div className='user-options'>
+              <p>My status</p>
+              <p>No updates</p>
+            </div>
         </div>
-        <span>Recent updates</span>
+        <span className='span'>Recent updates</span>
+        
       </div>
       <div className='status'>
         <div className='status-upload'>
             {/* <img className='status-image' alt='status-upload'>{picture}</img> */}
-            <img src={picture} alt="" width={'20px'} />
-        </div>
-        <div>
-            <p>{sender}</p>
-            {/* <p>Today, 4:24 PM</p> */}
+            <img src={picture} alt=""   className='upload'/>
+            <p className='names'>{sender}</p>
         </div>
       </div>
     </>
